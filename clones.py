2a14a7b3afd3ae00d7da70a76273dad1629be1cb
wangjue1006 <52403600+wangjue1006@users.noreply.github.com>
2020-08-07 00:08:05 +0800

    Delete clones.py

--- clones.py
+++
@@ -1,7 +0,0 @@
-import torch.nn as nn
-class Clones(nn.Module):
-    def __init__(self):
-        super(Clones,self).__init__()
-        pass
-    def forward(self,layer,n):
-        return nn.ModuleList([layer for i in range(n)])
