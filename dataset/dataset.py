ee13a157c048db5568ed45578305ecfe739a2a3d
wangjue1006 <52403600+wangjue1006@users.noreply.github.com>
2020-08-06 23:53:57 +0800

    Update dataset.py

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -5,31 +5,36 @@ import re
 cop = re.compile("[^\u4e00-\u9fa5^a-z^A-Z^0-9]")
 
 def sub_mask(size):
-    mask=(torch.triu(torch.ones(1,size,size))==1).transpose(-1,-2)
+    mask=(torch.triu(torch.ones(1,size,size),1)==0)
     return mask.unsqueeze(0)
 
 class Transformer_Dataset(Dataset):
-    def __init__(self,dataset,max_length,vocab):
+    def __init__(self,dataset,max_length,vocab,if_train):
         self.dataset=dataset
         self.vocab=vocab
+        self.if_train=if_train
         self.max_length=max_length
     def __len__(self):
         return len(self.dataset)
 
     def __getitem__(self, item):
         input_=jieba.lcut(cop.sub('',self.dataset[item][0]))
-        label=jieba.lcut(cop.sub('',self.dataset[item][1])) if len(item)==2 else None
+        label=jieba.lcut(cop.sub('',self.dataset[item][1])) if self.if_train else None
+
         input_i=[]
         label_i=[]
-
-        if len(item)==2:
+        if self.if_train:
             for word in label:
                 label_i.append(self.vocab.stoi[word])
-            label_i = [self.vocab.stoi['<sos>']] + label_i + [self.vocab.stoi['<eos>']]
+            tgt_i =label_i+[self.vocab.stoi['<eos>']]
+            label_i = [self.vocab.stoi['<sos>']] + label_i
+
             if len(label_i)<self.max_length:
                 label_i+=[self.vocab.stoi['<pad>'] for i in range(self.max_length-len(label_i))]
+                tgt_i+=[self.vocab.stoi['<pad>'] for i in range(self.max_length-len(tgt_i))]
             if len(label_i)>self.max_length:
                 label_i=label_i[:self.max_length]
+                tgt_i=tgt_i[:self.max_length]
             label_i = torch.LongTensor(label_i)
             tgt_mask = (label_i != self.vocab.stoi['<pad>']) & sub_mask(label_i.size(-1))
 
@@ -45,6 +50,7 @@ class Transformer_Dataset(Dataset):
 
         src_mask=(input_i!=self.vocab.stoi['<pad>']).unsqueeze(-2)
 
-        if len(item)==2:
-            return input_i,label_i,src_mask.unsqueeze(1),tgt_mask.squeeze(1)
-        return input_i,src_mask
+        if self.if_train:
+            return input_i,label_i,src_mask.unsqueeze(1),tgt_mask.squeeze(1),torch.LongTensor(tgt_i)
+        else:
+            return input_i,src_mask.unsqueeze(1)
