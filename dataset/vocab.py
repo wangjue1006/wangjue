99bd6ee914e6b7f885b0ac7b21da9f85a9b5f78e
wangjue1006 <52403600+wangjue1006@users.noreply.github.com>
2020-08-06 23:59:34 +0800

    Add files via upload

---
+++ dataset/vocab.py
@@ -0,0 +1,24 @@
+import jieba
+import re
+cop = re.compile("[^\u4e00-\u9fa5^a-z^A-Z^0-9]")
+class Vocab:
+    def __init__(self,max_size):
+        self.stoi={'<sos>':0,'<pad>':1,'<eos>':2}
+        self.itos=[]
+        self.max_size=max_size
+        for item in self.stoi:
+            self.itos.append(item)
+
+    def s2i(self,sentence):
+        words=jieba.lcut(cop.sub('',sentence))
+        for word in words:
+            if word not in self.stoi:
+                self.stoi[word]=len(self.itos)
+                self.itos.append(word)
+
+    def i2s(self,seq):
+        res=[]
+        for i in seq:
+            word=self.itos[int(i)]
+            res.append(word)
+        return ''.join(res)
