56d1a7953f2f2035cf9baa7c68d32c5dd376ff55
wangjue1006 <52403600+wangjue1006@users.noreply.github.com>
2020-08-24 11:18:41 +0800

    Update dataset.py

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -2,7 +2,7 @@ from torch.utils.data import Dataset
 import torch
 import jieba
 import re
-cop = re.compile("[^\u4e00-\u9fa5^a-z^A-Z^0-9]")
+
 
 def sub_mask(size):
     mask=(torch.triu(torch.ones(1,size,size),1)==0)
@@ -18,8 +18,8 @@ class Transformer_Dataset(Dataset):
         return len(self.dataset)
 
     def __getitem__(self, item):
-        input_=jieba.lcut(cop.sub('',self.dataset[item][0]))
-        label=jieba.lcut(cop.sub('',self.dataset[item][1])) if self.if_train else None
+        input_=self.dataset[item][0]
+        label=self.dataset[item][1]if self.if_train else None
 
         input_i=[]
         label_i=[]
@@ -27,13 +27,12 @@ class Transformer_Dataset(Dataset):
             for word in label:
                 label_i.append(self.vocab.stoi[word])
             tgt_i =label_i+[self.vocab.stoi['<eos>']]
-            label_i = [self.vocab.stoi['<sos>']] + label_i
 
-            if len(label_i)<self.max_length:
-                label_i+=[self.vocab.stoi['<pad>'] for i in range(self.max_length-len(label_i))]
+            if len(label_i)<self.max_length-1:
+                label_i+=[self.vocab.stoi['<pad>'] for i in range(self.max_length-1-len(label_i))]
                 tgt_i+=[self.vocab.stoi['<pad>'] for i in range(self.max_length-len(tgt_i))]
-            if len(label_i)>self.max_length:
-                label_i=label_i[:self.max_length]
+            if len(label_i)>self.max_length-1:
+                label_i=label_i[:self.max_length-1]
                 tgt_i=tgt_i[:self.max_length]
             label_i = torch.LongTensor(label_i)
             tgt_mask = (label_i != self.vocab.stoi['<pad>']) & sub_mask(label_i.size(-1))
